a9519489d5ffc3a75728409b12b2056beb321606
coder755 <coderjamespark@gmail.com>
2024-10-21 13:38:21 -0600

    Move to its own repo

--- infrastructure/src/Infrastructure/Database/DatabaseStackProps.cs
+++
@@ -1,9 +0,0 @@
-﻿using Amazon.CDK;
-using Amazon.CDK.AWS.EC2;
-
-namespace Infrastructure.Database;
-
-public class DatabaseStackProps : StackProps
-{
-    public IVpc Vpc { get; set; }
-}
--- infrastructure/src/Infrastructure/PubSub/PubSubStackProps.cs
+++
@@ -1,9 +0,0 @@
-﻿using Amazon.CDK;
-using Amazon.CDK.AWS.EC2;
-
-namespace Infrastructure.PubSub;
-
-public class PubSubStackProps : StackProps
-{
-    public IVpc Vpc { get; set; }
-}
